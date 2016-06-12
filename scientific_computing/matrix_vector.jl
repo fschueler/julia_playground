fa9c136b5e673123f2a46ade2fa9f793d43ed97e
fschueler <fschueler@posteo.de>
2016-06-13 00:13:18 +0200

    Add operations for tridiagonal matrices

--- scientific_computing/matrix_vector.jl
+++ scientific_computing/matrix_vector.jl
@@ -154,13 +154,20 @@ function matvec_c5(A, x, n)
 end
 
 # colwise, 2d blocked, unrolled
-function matvec_c5(A, x, n)
+function matvec_c6(A, x, n)
   y = zeros(eltype(x), n)
   for jj = 1:BL:n
     for ii = 1:BL:n
       for j = jj:(jj+BL-1)
-        for i = ii:(ii+BL-1)
+        for i = ii:8:(ii+BL-1)
           y[i] = y[i] + A[i,j] * x[j]
+          y[i+1] = y[i+1] + A[i+1,j] * x[j]
+          y[i+2] = y[i+2] + A[i+2,j] * x[j]
+          y[i+3] = y[i+3] + A[i+3,j] * x[j]
+          y[i+4] = y[i+4] + A[i+4,j] * x[j]
+          y[i+5] = y[i+5] + A[i+5,j] * x[j]
+          y[i+6] = y[i+6] + A[i+6,j] * x[j]
+          y[i+7] = y[i+7] + A[i+7,j] * x[j]
         end
       end
     end
@@ -182,6 +189,65 @@ function matvec_cp(A, x, n)
   end
   y
 end
+
+# columnwise parallel with reduce
+function matvec_cp2(A, x, n)
+  y = zeros(eltype(x), n)
+  y = @parallel (+) for j = 1:n
+    z = zeros(eltype(x), n)
+    for i = 1:n
+      z[i] = A[i,j] * x[j]
+    end
+    z
+  end
+  y
+end
+
+################################################
+#          sparse matrices                     #
+################################################
+
+# tridiagonal sequential
+function matvec_tr(A, x, n)
+  y = zeros(eltype(x), n)
+  y[1] = A[1,1] * x[1] + A[1,2] * x[2]
+  for i = 2:n-1
+    y[i] = A[i, i-1] * x[i-1] + A[i, i] * x[i] + A[i, i+1] * x[i+1]
+  end
+  y[n] = A[n, n-1] * x[n-1] + A[n,n] * x[n]
+  y
+end
+
+# tridiagonal with branches
+function matvec_tr2(A, x, n)
+  y = zeros(eltype(x), n)
+  for i = 1:n
+    if (i == 1)
+      y[1] = A[1,1] * x[1] + A[1,2]
+    elseif (i == n)
+      y[n] = A[n, n-1] * x[n-1] + A[n,n] * x[n]
+    else
+      y[i] = A[i, i-1] * x[i-1] + A[i, i] * x[i] + A[i, i+1] * x[i+1]
+    end
+  end
+  y
+end
+
+# tridiagonal parallelized with branches
+function matvec_trp(A, X, n)
+  y = zeros(eltype(x), n)
+  @parallel for i = 1:n
+    if (i == 1)
+      y[1] = A[1,1] * x[1] + A[1,2]
+    elseif (i == n)
+      y[n] = A[n, n-1] * x[n-1] + A[n,n] * x[n]
+    else
+      y[i] = A[i, i-1] * x[i-1] + A[i, i] * x[i] + A[i, i+1] * x[i+1]
+    end
+  end
+  y
+end
+
 
 function timeit(n, reps)
   y = zeros(Float64, n)
@@ -259,15 +325,45 @@ function timeit(n, reps)
   end
   println("matvec_c (bl, 2d)    | ",time/reps)
 
+  # col-wise y = Ax (blocked 2d, unrolled)
+  time = @elapsed for j in 1:reps
+      y += matvec_c6(A, x, n)
+  end
+  println("matvec_c (bl, 2d, un)| ",time/reps)
+
   # col-wise y = Ax (blocked 2d, parallel)
   time = @elapsed for j in 1:reps
       y += matvec_cp(A, x, n)
   end
   println("matvec_c (par, 2db)  | ",time/reps)
+
+  # col-wise y = Ax (parallel)
+  time = @elapsed for j in 1:reps
+      y += matvec_cp2(A, x, n)
+  end
+  println("matvec_c (par)       | ",time/reps)
+
+  # tridiagonal
+  time = @elapsed for j in 1:reps
+      y += matvec_tr(A, x, n)
+  end
+  println("matvec_tr            | ",time/reps)
+
+  # tridiagonal ifelse
+  time = @elapsed for j in 1:reps
+      y += matvec_tr(A, x, n)
+  end
+  println("matvec_tr (branch)   | ",time/reps)
+
+  # tridiagonal
+  time = @elapsed for j in 1:reps
+      y += matvec_tr(A, x, n)
+  end
+  println("matvec_tr (parallel) | ",time/reps)
 end
 
 const N  = 2^12   # number of elements of x, y, A is N*N
 const CL = 8      # cache-line length in Float64
 const BL = div(N,CL) # block-length for blocked algorithms
 
-timeit(N, 100)
+timeit(N, 25)
