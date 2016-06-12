b5ac2b893308c9ec6c4f3c0851bb73c08cb8b329
fschueler <fschueler@posteo.de>
2016-06-12 18:32:26 +0200

    Add/fix some operations

--- scientific_computing/matrix_vector.jl
+++ scientific_computing/matrix_vector.jl
@@ -19,7 +19,7 @@ function matvec_r2(A, x, n)
   for ii = 1:BL:n
     for j = 1:n
       for i = ii:(ii+BL-1)
-        y[i] = A[i,j] * x[j]
+        y[i] = y[i] + A[i,j] * x[j]
       end
     end
   end
@@ -32,14 +32,14 @@ function matvec_r3(A, x, n)
   for ii = 1:BL:n
     for j = 1:n
       for i = ii:8:(ii+BL-1)
-        y[i] = A[i,j] * x[j]
-        y[i+1] = A[i+1,j] * x[j]
-        y[i+2] = A[i+2,j] * x[j]
-        y[i+3] = A[i+3,j] * x[j]
-        y[i+4] = A[i+4,j] * x[j]
-        y[i+5] = A[i+5,j] * x[j]
-        y[i+6] = A[i+6,j] * x[j]
-        y[i+7] = A[i+7,j] * x[j]
+        y[i] = y[i] + A[i,j] * x[j]
+        y[i+1] = y[i+1] + A[i+1,j] * x[j]
+        y[i+2] = y[i+2] + A[i+2,j] * x[j]
+        y[i+3] = y[i+3] + A[i+3,j] * x[j]
+        y[i+4] = y[i+4] + A[i+4,j] * x[j]
+        y[i+5] = y[i+5] + A[i+5,j] * x[j]
+        y[i+6] = y[i+6] + A[i+6,j] * x[j]
+        y[i+7] = y[i+7] + A[i+7,j] * x[j]
       end
     end
   end
@@ -78,7 +78,7 @@ function matvec_rp2(A, x, n)
   @parallel for ii = 1:BL:n
     for j = 1:n
       for i = ii:(ii+BL-1)
-        y[i] = A[i,j] * x[j]
+        y[i] = y[i] + A[i,j] * x[j]
       end
     end
   end
@@ -103,7 +103,7 @@ function matvec_c2(A, x, n)
   y = zeros(eltype(x), n)
   for j = 1:n
     for i = 1:n
-      y[i] = A[i,j] * x[j]
+      y[i] = y[i] + A[i,j] * x[j]
     end
   end
   y
@@ -114,7 +114,7 @@ function matvec_c3(A, x, n)
   y = zeros(eltype(x), n)
   for j = 1:n
     @simd for i = 1:n
-      y[i] = A[i,j] * x[j]
+      y[i] = y[i] + A[i,j] * x[j]
     end
   end
   y
@@ -125,20 +125,35 @@ function matvec_c4(A, x, n)
   y = zeros(eltype(x), n)
   for j = 1:n
     for i = 1:8:n
-      y[i] = A[i,j] * x[j]
-      y[i+1] = A[i+1,j] * x[j]
-      y[i+2] = A[i+2,j] * x[j]
-      y[i+3] = A[i+3,j] * x[j]
-      y[i+4] = A[i+4,j] * x[j]
-      y[i+5] = A[i+5,j] * x[j]
-      y[i+6] = A[i+6,j] * x[j]
-      y[i+7] = A[i+7,j] * x[j]
+      y[i] = y[i] + A[i,j] * x[j]
+      y[i+1] = y[i+1] + A[i+1,j] * x[j]
+      y[i+2] = y[i+2] + A[i+2,j] * x[j]
+      y[i+3] = y[i+3] + A[i+3,j] * x[j]
+      y[i+4] = y[i+4] + A[i+4,j] * x[j]
+      y[i+5] = y[i+5] + A[i+5,j] * x[j]
+      y[i+6] = y[i+6] + A[i+6,j] * x[j]
+      y[i+7] = y[i+7] + A[i+7,j] * x[j]
     end
   end
   y
 end
 
 # colwise, 2d blocked
+function matvec_c5(A, x, n)
+  y = zeros(eltype(x), n)
+  for jj = 1:BL:n
+    for ii = 1:BL:n
+      for j = jj:(jj+BL-1)
+        for i = ii:(ii+BL-1)
+          y[i] = y[i] + A[i,j] * x[j]
+        end
+      end
+    end
+  end
+  y
+end
+
+# colwise, 2d blocked, unrolled
 function matvec_c5(A, x, n)
   y = zeros(eltype(x), n)
   for jj = 1:BL:n
@@ -182,77 +197,77 @@ function timeit(n, reps)
   time = @elapsed for j in 1:reps
     y += matvec_r(A, x, n)
   end
-  println("matvec_r             = ",time)
+  println("matvec_r             | ",time/reps)
 
   # row-wise y = Ax (blocked, becomes effectively col-wise)
   time = @elapsed for j in 1:reps
       y += matvec_r2(A, x, n)
   end
-  println("matvec_r (blocked)   = ",time)
+  println("matvec_r (blocked)   | ",time/reps)
 
   # row-wise y = Ax (blocked, unrolled)
   time = @elapsed for j in 1:reps
       y += matvec_r3(A, x, n)
   end
-  println("matvec_r (bl, unr)   = ",time)
+  println("matvec_r (bl, unr)   | ",time/reps)
 
   # row-wise y = Ax (blocked 2d)
   time = @elapsed for j in 1:reps
       y += matvec_r4(A, x, n)
   end
-  println("matvec_r (bl, 2d)    = ",time)
+  println("matvec_r (bl, 2d)    | ",time/reps)
 
   # row-wise y = Ax (parallel)
   time = @elapsed for j in 1:reps
       y += matvec_rp(A, x, n)
   end
-  println("matvec_r (par)       = ",time)
+  println("matvec_r (par)       | ",time/reps)
 
   # row-wise y = Ax (blocked, parallel)
   time = @elapsed for j in 1:reps
       y += matvec_rp2(A, x, n)
   end
-  println("matvec_r (bl, par)   = ",time)
+  println("matvec_r (bl, par)   | ",time/reps)
 
 # col-wise y = Ax (one loop)
   time = @elapsed for j in 1:reps
       y += matvec_c1(A, x, n)
   end
-  println("matvec_c (1 loop)    = ",time)
+  println("matvec_c (1 loop)    | ",time/reps)
 
 # col-wise y = Ax (two loops)
   time = @elapsed for j in 1:reps
       y += matvec_c2(A, x, n)
   end
-  println("matvec_c (2 loops)   = ",time)
+  println("matvec_c (2 loops)   | ",time/reps)
 
   # col-wise y = Ax (simd)
   time = @elapsed for j in 1:reps
       y += matvec_c3(A, x, n)
   end
-  println("matvec_c (simd)      = ",time)
+  println("matvec_c (simd)      | ",time/reps)
 
   # col-wise y = Ax (unrolled)
   time = @elapsed for j in 1:reps
       y += matvec_c4(A, x, n)
   end
-  println("matvec_c (unrolled)  = ",time)
+  println("matvec_c (unrolled)  | ",time/reps)
 
   # col-wise y = Ax (blocked 2d)
   time = @elapsed for j in 1:reps
       y += matvec_c5(A, x, n)
   end
-  println("matvec_c (bl, 2d)    = ",time)
+  println("matvec_c (bl, 2d)    | ",time/reps)
 
   # col-wise y = Ax (blocked 2d, parallel)
   time = @elapsed for j in 1:reps
       y += matvec_cp(A, x, n)
   end
-  println("matvec_c (par, 2db)  = ",time)
+  println("matvec_c (par, 2db)  | ",time/reps)
 end
 
 const N  = 2^12   # number of elements of x, y, A is N*N
 const CL = 8      # cache-line length in Float64
 const BL = div(N,CL) # block-length for blocked algorithms
 
-timeit(N, 10)
+timeit(N, 100)
